9e6ad0f3c666027ae430912722edec5395225da6
wwq0327 <wwq0327@gmail.com>
2015-06-24 08:20:47 +0800

    项目初始化。

---
+++ Zhao Mengfu Shufa/Zhao Mengfu Shufa/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Zhao Mengfu Shufa
+//
+//  Created by wyatt on 15/6/24.
+//  Copyright (c) 2015年 Wanqing Wang. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
